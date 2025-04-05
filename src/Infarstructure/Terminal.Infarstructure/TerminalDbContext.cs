fd599995e8423dd4e277a1467766cb1b4b0882c6
alirezadoostdar <alireza.doostdar@gmail.com>
2025-04-05 14:39:56 +0200

    create terminal db context

--- src/Infarstructure/Terminal.Infarstructure/TerminalDbContext.cs
+++ src/Infarstructure/Terminal.Infarstructure/TerminalDbContext.cs
@@ -1,12 +1,29 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 using System.Text;
 using System.Threading.Tasks;
+using Terminal.Domain.Entities;
 
 namespace Terminal.Infarstructure;
 
-public class TerminalDbContext
+public class TerminalDbContext : DbContext
 {
+    public TerminalDbContext(DbContextOptions options):base(options)
+    {
+        
+    }
 
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+       
+    }
+
+    public DbSet<Bus> Buses { get; set; }
+    public DbSet<Route> Routes { get; set; }
+    public DbSet<Trip> Trips { get; set; }
+    public DbSet<Ticket> Tickets { get; set; }
 }
